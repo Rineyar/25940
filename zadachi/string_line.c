226e8ea966c04b8cbbfcb82d8c81205714f821d5
Rineyar <radomirbabich22@mail.ru>
2026-09-23 13:45:30 +0700

    Линия

---
+++ zadachi/string_line.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+//gcc -Wall -Wextra -Wpedantic -g string_line.c -o res
+int main(void)
+{
+    char line[1024];
+    int i = 0;
+
+    char **lines = malloc(sizeof(char*) * 4);
+    int cap = 4;
+    int len = 0;
+
+    while(1)
+    {
+        int ch = getchar();
+
+        if(ch == '.')
+        {
+            break;
+        }
+
+        while(ch != EOF && ch != '\n')
+        {
+            line[i++] = ch;
+
+            ch = getchar();
+        }
+
+        line[i++] = '\n';
+        line[i++] = '\0';
+
+        lines[len] = malloc(sizeof(char) * (i + 2));
+
+        memcpy(lines[len++], line, i * sizeof(char));
+
+        i = 0;
+
+        if(len == cap)
+        {
+            cap *= 2;
+
+            lines = realloc(lines, cap);
+        }
+    }
+
+    for(int ii = 0; ii < len; ii++)
+    {
+        printf("%s", lines[ii]);
+    }
+
+    return 0;
+}
